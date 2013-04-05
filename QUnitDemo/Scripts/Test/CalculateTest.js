529b5e7445569ec864fca0f8d6415aad40920568
Dilan Arandara <dilan.madu@gmail.com>
2013-04-05 17:00:55 +0530

    Finalized the code.

---
+++ QUnitDemo/Scripts/Test/CalculateTest.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../calculate.js" />
+/// <reference path="../qunit-1.11.0.js" />
+
+test("Calculate Test", function () {
+    var calculateVM = new CalculateVM();
+    calculateVM.num1 = 10;
+    calculateVM.num2 = 20;
+
+    calculateVM.calculate();
+
+    equal(calculateVM.total(), 30, "num1 : 10, num2 : 20, total expected : 30");
+});
---
+++ QUnitDemo/Scripts/calculate.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="knockout-2.2.1.debug.js" />
+
+var CalculateVM = function CalculateVM() {
+    var self = this;
+
+    self.num1 = 0;
+    self.num2 = 0;
+    self.total = ko.observable(0);
+
+    self.calculate = function () {
+        self.total(parseInt(self.num1) + parseInt(self.num2));
+    }
+}
+
+ko.applyBindings(new CalculateVM());
